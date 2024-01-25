97b2dff5cded94d68ca0ab04cad47f5a8f219f17
Sanghoon Lee <nasi3611@naver.com>
2024-01-25 10:55:26 +0900

    Feat : 스켈레톤 예시 코드 분리

---
+++ smartGYM/lib/page/home/gym_list/skeletons.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:skeletons/skeletons.dart';
+
+import '../../../model/machine_data.dart';
+import 'gym_item.dart';
+
+
+/// 스켈레톤 예시 코드
+class SkeletonList extends StatefulWidget {
+  final List<GymMachineItem> machines;
+
+  SkeletonList({required this.machines});
+
+  @override
+  State<SkeletonList> createState() => _SkeletonListState();
+
+}
+
+
+
+class _SkeletonListState extends State<SkeletonList> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 데이터 가져오기를 시뮬레이션하기 위한 지연
+    Future.delayed(const Duration(seconds: 3), () {
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading ? const SkeletonsWidget() : ListView.builder(
+      itemCount: widget.machines.length,
+      itemBuilder: (context, index) {
+        final machine = widget.machines[index];
+        return GymMachineListItem(machine: machine);
+      },
+    );
+  }
+}
+
+class SkeletonsWidget extends StatelessWidget {
+  const SkeletonsWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) => Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Container(
+      padding: const EdgeInsets.all(8.0),
+      decoration: const BoxDecoration(color: Colors.white),
+      child: SkeletonItem(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const SkeletonAvatar(
+                  style: SkeletonAvatarStyle(
+                      shape: BoxShape.circle, width: 50, height: 50),
+                ),
+                SizedBox(width: 8),
+                Expanded(
+                  child: SkeletonParagraph(
+                    style: SkeletonParagraphStyle(
+                        lines: 3,
+                        spacing: 6,
+                        lineStyle: SkeletonLineStyle(
+                          randomLength: true,
+                          height: 10,
+                          borderRadius: BorderRadius.circular(8),
+                          minLength: MediaQuery.of(context).size.width / 6,
+                          maxLength: MediaQuery.of(context).size.width / 3,
+                        )),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 12),
+            SkeletonParagraph(
+              style: SkeletonParagraphStyle(
+                  lines: 3,
+                  spacing: 6,
+                  lineStyle: SkeletonLineStyle(
+                    randomLength: true,
+                    height: 10,
+                    borderRadius: BorderRadius.circular(8),
+                    minLength: MediaQuery.of(context).size.width / 2,
+                  )),
+            ),
+            const SizedBox(height: 12),
+            SkeletonAvatar(
+              style: SkeletonAvatarStyle(
+                width: double.infinity,
+                minHeight: MediaQuery.of(context).size.height / 8,
+                maxHeight: MediaQuery.of(context).size.height / 3,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Row(
+                  children: [
+                    SkeletonAvatar(
+                        style: SkeletonAvatarStyle(width: 20, height: 20)),
+                    SizedBox(width: 8),
+                    SkeletonAvatar(
+                        style: SkeletonAvatarStyle(width: 20, height: 20)),
+                    SizedBox(width: 8),
+                    SkeletonAvatar(
+                        style: SkeletonAvatarStyle(width: 20, height: 20)),
+                  ],
+                ),
+                SkeletonLine(
+                  style: SkeletonLineStyle(
+                      height: 16,
+                      width: 64,
+                      borderRadius: BorderRadius.circular(8)),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    ),
+          ),
+        );
+  }
+}
