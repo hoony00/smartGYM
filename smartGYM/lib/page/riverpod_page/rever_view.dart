29aa43fec017e442bb880797cb9990548dd1fa2c
Sanghoon Lee <nasi3611@naver.com>
2024-01-30 10:31:17 +0900

    Feat : 상태관리 추가

--- smartGYM/lib/page/riverpod_page/rever_view.dart
+++ smartGYM/lib/page/riverpod_page/rever_view.dart
@@ -11,34 +11,23 @@ class RiverView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final String value = ref.watch(helloWorldProvider);
-    // final String testValue = ref.watch(testIntegerProvider) as String;
 
     ref.listen(counterProvider, (previousState, newState) {
       print("The new value is $newState");
     });
 
-    final state = ref.watch(todoProvider);
-    print("state.count : ${state.count}");
+
 
     return Scaffold(
       body: Center(
         child: Column(
           children: [
-            if (state.todos != null)
-              ...List.generate(
-                state.count,
-                    (index) => Text(state.todos![index].title),
-              ).toList(),
-
             ElevatedButton(onPressed: () {
-              ref
-                  .read(todoProvider.notifier)
-                  .update((state) => state.copyWith(newTodo: Todo(title: 'test')));
 
 
                   }, child: const Text('할일 추가')),
             ElevatedButton(onPressed: () {
-              state.remove("test");
+
             }, child: const Text('할일 제거')),
           ],
         ),
